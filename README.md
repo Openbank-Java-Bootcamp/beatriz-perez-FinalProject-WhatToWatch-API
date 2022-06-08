ef8060463dda57baf949be432b18714befbfe411
beatriz-perez <beatriz.pfp@gmail.com>
2022-06-08 10:22:07 +0200

    Upload initial project structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# What To Watch - REST API
+
+A working REST API, built by Beatriz Pérez using Spring Boot that runs on a server.
+
+## API documentation
---
+++ src/main/java/com/ironhack/WTWAPI/WtwApiApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.WTWAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtwApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtwApiApplication.class, args);
+	}
+
+}
